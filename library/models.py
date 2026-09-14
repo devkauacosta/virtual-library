dabbf90a3430cb368d01cc22994a5b4b83390937
nkzprog <costaferreirakaua03@gmail.com>
2026-09-14 18:18:36 -0300

    add Model: Publisher

--- library/models.py
+++ library/models.py
@@ -7,3 +7,11 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.text_genre
+
+# Tabela de editoras
+class Publisher(models.Model):
+    id = models.AutoField(primary_key=True)
+    name_publisher = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name

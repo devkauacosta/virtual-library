9e620bd9fdd8088b45289445159e0d0213eddd6e
nkzprog <costaferreirakaua03@gmail.com>
2026-09-14 18:15:45 -0300

    add model: Genre

--- library/models.py
+++ library/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Tabela de Gêneros Literarios
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    text_genre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text_genre

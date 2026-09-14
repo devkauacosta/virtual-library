466973397b3ed510c1944fc3d0009bef6040fd86
nkzprog <costaferreirakaua03@gmail.com>
2026-09-14 18:45:39 -0300

    Create the BookListView with search filter

--- library/views.py
+++ library/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book
 
-# Create your views here.
+class BookListView(ListView):
+    """
+    Retorna uma lista de todos objetos da tabela Book
+    """
+
+    model = Book
+    template_name = 'books.html'
+    context_object_name = 'books'
+
+    #Filtro de busca
+    def get_queryset(self):
+        books = super().get_queryset().order_by('title')
+        search = self.request.GET.get('search')
+        if search:
+            books = books.filter(model_contains=search)
+        return books
